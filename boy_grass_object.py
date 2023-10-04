600a16f3494102a769e2de005e9e5cf17f1a69f0
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-10-04 20:26:17 +0900

    Draw 20 random small or big ball in screen

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import random
 
+
 # Game object class here
 class Grass:
     def __init__(self):
@@ -11,17 +12,41 @@ class Grass:
 
     def update(self): pass
 
+
 class Boy:
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
         self.frame = 0
         self.image = load_image('run_animation.png')
+
     def update(self):
         self.frame = random.randint(0, 7)
         self.x += 5
 
     def draw(self):
-        self.image.clip_draw(self.frame*100,0,100,100,self.x, self.y)
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(50, 750), 599
+        self.speed = random.randint(1, 15)
+        self.type = random.randint(1, 2)
+
+        if self.type == 1:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+
+    def update(self):
+        self.y -= 3 * self.speed
+
+    def draw(self):
+        if self.type == 1:
+            self.image.draw(self.x, self.y,43, 43)
+        else:
+            self.image.draw(self.x, self.y,23, 23)
+
 
 def handle_events():
     global running
@@ -37,6 +62,7 @@ def reset_world():
     global running
     global grass
     global team
+    global ball
     global world
 
     running = True
@@ -48,20 +74,21 @@ def reset_world():
     team = [Boy() for i in range(11)]
     world += team
 
+    ball = [Ball() for i in range(20)]
+    world += ball
+
+
 def update_world():
-    grass.update()
     for o in world:
         o.update()
 
 
-
 def render_world():
     clear_canvas()
-    grass.draw()
     for o in world:
         o.draw()
-    update_canvas()
 
+    update_canvas()
 
 open_canvas()
 
